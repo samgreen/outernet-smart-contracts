6f00b436f30b65bd36e1a78ddbd91a0e4b6cc789
Null Pointer <nullpointer@lavamail.com>
2020-07-17 20:01:31 -0700

    Update contracts

--- migrations/3_outernet_migration.js
+++ migrations/3_outernet_migration.js
@@ -3,5 +3,5 @@ const ElderCoin = artifacts.require("./ElderCoin.sol");
 module.exports = async function(deployer, network, accounts) {
   await deployer.deploy(ElderCoin)
   const elderCoin = await ElderCoin.deployed()
-  elderCoin.mint(accounts[0], 1, "https://example.com/ElderCoin.json")
+  elderCoin.mint(accounts[0], 1000, "https://example.com/ElderCoin.json")
 };
